f57c6e3cb5a370228e075a3805b6dc78b8acc0f4
Evan <evan.straw99@gmail.com>
2017-12-25 09:21:37 -0600

    create basic functionality for opening and parsing file
    Also updated package.json

--- ituneslib.js
+++ ituneslib.js
@@ -2,23 +2,37 @@ var fs = require('fs');
 var Promise = require('promise');
 var plist = require('plist');
 
-function ituneslib(filename){
+module.exports = function ituneslib() {
+  var data;
+  var ready = false;
 
   //function to make sure we're given a valid file
   function validateFilename(fname) {
     //will fail if filename is null or not a string, file doesn't exist, or file is a directory
-    return (fname!==null && typeof fname==='string' && fs.existsSync(fname) && !fs.lstatSync(fname).isDirectory());
+    return (fname !== null && typeof fname === 'string' && fs.existsSync(fname) && !fs.lstatSync(fname).isDirectory());
   }
 
   //function to reformat all the keys from the plist file to not be strings with spaces and stuff in them
   function reformat_keys(data) {
-    Object.keys(data);
+    Object.keys(data).forEach(function(key){
+      var value = data[key];
+      if(typeof value==='object') {
+        reformat_keys(value);
+      }
+      delete data[key];
+      var newkey = key.toLowerCase().replace(/\s/,'');
+      data[newkey] = value;
+    });
   }
-  
-  this.open = function open() {
-    return new Promise(function(fulfill,reject) {
-      fs.readFile(this.filename, function(err, dat) {
-        if(err) {
+
+  //opens an itunes library xml file and reads and reformats the data
+  this.open = function open(filename) {
+    if (!validateFilename(filename)) {
+      throw 'Invalid file path!';
+    }
+    return new Promise(function (fulfill, reject) {
+      fs.readFile(filename, function (err, dat) {
+        if (err) {
           //if there's an error reading the file, reject the promise
           reject(err);
         }
@@ -27,8 +41,11 @@ function ituneslib(filename){
             var xmlData = dat.toString();
             xmlData = xmlData.replace(/[\n\t\r]/g, '');
             data = plist.parse(xmlData);
+            reformat_keys(data);
+            ready = true;
+            fulfill();
           }
-          catch(err) {
+          catch (err) {
             //if any errors thrown, reject the promise
             reject(err);
           }
@@ -36,21 +53,14 @@ function ituneslib(filename){
       });
     });
   };
-    
-  var data;
- 
-  if(!validateFilename()) {
-    throw 'Invalid file path!';
-  }
-  else
-  {
-    this.filename = filename;
-  }
-  
 
-  
-  
-  
+  this.getRawData = function getRawData() {
+    return data;
+  };
+
+
+
+
 }
 
 
