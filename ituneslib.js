0a86ce50ae3891ba7473392696bf930dc5a1f21f
Evan <evan.straw99@gmail.com>
2017-12-26 14:55:50 -0600

    create functions to get individual tracks and all tracks

--- ituneslib.js
+++ ituneslib.js
@@ -5,6 +5,7 @@ var plist = require('plist');
 module.exports = function ituneslib() {
   var data;
   var ready = false;
+  let t = this;
 
   //function to make sure we're given a valid file
   function validateFilename(fname) {
@@ -59,7 +60,8 @@ module.exports = function ituneslib() {
   };
 
   this.Track = function Track(trackData) {
-    var track_keys = [
+    //list of all the properties that an iTunes library track will have
+    var properties = [
       'track_id',
       'size',
       'total_time',
@@ -79,13 +81,93 @@ module.exports = function ituneslib() {
       'location'
     ];
     if (trackData != null && typeof trackData === 'object') {
-      for (var i = 0; i < track_keys.length; i++) {
-        var key = track_keys[i];
-        if (trackData[key] !== null) {
-          this[key] = trackData[key];
-        }
+      //go through all the valid properties and assign them to our new Track object
+      //this makes sure that a returned Track object always has the same properties and doesn't have any extras
+      for (var i = 0; i < properties.length; i++) {
+        //variable to hold current property
+        var prop = properties[i];
+        //set the current property's value to the new Trap object
+        this[prop] = trackData[prop];
+
       }
     }
+  };
+
+  this.getTracks = function getTracks() {
+    var getTrackByIDSync = this.getTrackByIDSync;
+    return new Promise(function (fulfill, reject) {
+      if (ready) {
+        try {
+          var output = [];
+          var keys = Object.keys(data.tracks);
+          for (var i = 0; i < keys.length; i++) {
+            var currentKey = keys[i];
+            output.push(getTrackByIDSync(currentKey));
+          }
+          fulfill(output);
+        }
+        catch (e) {
+          reject(e);
+        }
+      }
+      else {
+        reject(new Error("No data ready (call open() first)!"));
+      }
+    })
+  };
+
+  this.getTrackByIDSync = function getTrackByIDSync(id) {
+    var track = t.Track;
+    if (ready) {
+      if (id !== null) {
+        if (data.tracks[id]) {
+          var tdata = data.tracks[id];
+          return new track(tdata);
+        }
+        else {
+          throw new Error("No track found for the specified id!");
+        }
+      }
+      else {
+        throw new Error("Track ID is null!");
+      }
+    }
+    else {
+      throw new Error("No data ready (call open() first)!");
+    }
+  };
+
+  this.getTrackByID = function getTrackByID(id) {
+    var track = t.Track;
+    return new Promise(function (fulfill, reject) {
+      if (ready) {
+        if (id !== null) {
+          if (data.tracks[id]) {
+            try {
+              var tdata = data.tracks[id];
+              var t = new track(tdata);
+              fulfill(t);
+            }
+            catch (e) {
+              reject(e);
+            }
+          }
+          else {
+            reject(new Error("No track found for the specified id!"));
+          }
+        }
+        else {
+          reject(new Error("Track ID is null!"));
+        }
+      }
+      else {
+        reject(new Error("No data ready (call open() first)!"));
+      }
+    })
+  };
+
+  this.Playlist = function Playlist(playlistData) {
+
   };
 
   this.getMajorVersion = function getMajorVersion() {
@@ -105,6 +187,5 @@ module.exports = function ituneslib() {
       return data.application_version;
     }
   };
-};
-
-
+}
+;
